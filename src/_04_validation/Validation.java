342d1816668ddad3f51de78ed634ce8882c9a157
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-11-14 13:29:13 -0800

    vali

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -10,14 +10,38 @@ import javax.swing.JOptionPane;
 public class Validation {
 	public static void main(String[] args) {
 		
-		Random randomMaker = new Random();
+		Random gen = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int randomNumber = gen.nextInt(11);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 1) {
+			
+		} else if (randomNumber == 2) {
+			
+		} else if (randomNumber == 3) {
+			
+		} else if (randomNumber == 4) {
+			
+		} else if (randomNumber == 5) {
+			
+		} else if (randomNumber == 6) {
+			
+		} else if (randomNumber == 7) {
+			
+		} else if (randomNumber == 8) {
+			
+		} else if (randomNumber == 9) {
+			
+		} else if (randomNumber == 10) {
+			
+		} else {
+			
+		}
+			
+		
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
